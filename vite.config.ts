21976b36aea841eea6f09abd4142108501b2183e
Devon <83571687+evve21@users.noreply.github.com>
2026-01-02 18:30:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/macro-app/', // Nama repo baru kamu
-})
+export default defineConfig(({ mode }) => {
+    // Load env file dari folder root
+    const env = loadEnv(mode, process.cwd(), '');
+    
+    return {
+      plugins: [react()],
+      base: '/macro-app/', // Nama repository kamu
+      define: {
+        // Ini memastikan app tidak error jika API_KEY kosong saat build
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || '')
+      },
+      resolve: {
+        alias: {
+          // Mengarahkan @ ke folder utama
+          '@': path.resolve(__dirname, './'),
+        }
+      },
+      build: {
+        outDir: 'dist',
+      }
+    };
+});
