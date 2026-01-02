f3462fcebf1b4f96b4fc0336cbc82d32773569a5
Devon <83571687+evve21@users.noreply.github.com>
2026-01-02 18:32:26 +0800

    Refactor vite.config.ts for simplification

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,10 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    // Load env file dari folder root
-    const env = loadEnv(mode, process.cwd(), '');
-    
-    return {
-      plugins: [react()],
-      base: '/macro-app/', // Nama repository kamu
-      define: {
-        // Ini memastikan app tidak error jika API_KEY kosong saat build
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || '')
-      },
-      resolve: {
-        alias: {
-          // Mengarahkan @ ke folder utama
-          '@': path.resolve(__dirname, './'),
-        }
-      },
-      build: {
-        outDir: 'dist',
-      }
-    };
-});
+export default defineConfig({
+  plugins: [react()],
+  base: '/macro-app/',
+  define: {
+    'process.env': {}
+  }
+})
